85eef2d4e82802b612fe50c1cc682154729f23a4
Sam Lando <samuel.lando@gmail.com>
2018-10-21 22:49:25 +0300

    add .vscode to gitignore and changes to .travis.yml and app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,5 @@ from instance.config import app_config
 def create_app(config_name):
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_pyfile('config.py')
+
+    return app
